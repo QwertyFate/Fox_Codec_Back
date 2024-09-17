73bbbb729db7867454cb2930003c64f2bd06bc01
nafaryusnarga <nafaryusnarga@gmail.com>
2024-09-17 23:19:51 +0800

    changed connection to pooling

--- api/mysqldb.js
+++ api/mysqldb.js
@@ -1,22 +1,27 @@
 const bcrypt = require("bcrypt");
 require("dotenv").config();
 const mysql = require("mysql2/promise");
-let connection;
+let pool;
 let userArr = [];
 const randomSalt = 11;
 let userID;
 
 const ConnectingtoDB = async () => { 
-    if (!connection || connection.connection._closing) {
+    if (!pool) {
         try{
-            connection = await mysql.createConnection({
+            pool = await mysql.createPool({
                 host: 'database-1.cnkcq6sga73v.us-east-1.rds.amazonaws.com',
                 user: 'admin',
                 password: process.env.SQL_PASSWORD,
                 database: 'Chatappdb',
-                port: '3306'
+                port: '3306',
+                connectionLimit: 10,
+                waitForConnections: true,
+                queueLimit:0,
+                connectTimeout: 10000, // 10 seconds timeout for connecting
+
                 });
-                console.log("connected")
+                console.log("connected pool")
             } catch (error) {
                 console.error('Error connecting to the database:', error);
             }       
@@ -25,9 +30,11 @@ const ConnectingtoDB = async () => {
 }
 const findAll = async (userID) => {
     userArr = [];
+    let connection
     try {
-        const mysql = 'SELECT * FROM `Users`';
-        const [rows, fields] = await connection.query(mysql);
+        connection = await pool.getConnection();
+        const query = 'SELECT * FROM `Users`';
+        const [rows] =  await connection.query(query);
         for(let i = 0; i< rows.length; i++){
             if(rows[i].id != userID){
                     userArr.push({
@@ -41,47 +48,70 @@ const findAll = async (userID) => {
       return(userArr);  
       } catch (err) {
         console.log(err);
-      }
+      } finally {if (connection) connection.release();}
 }
 
 const RegisterNewUser = async (username, password) => {
     const passwordHash = await bcrypt.hashSync(password, randomSalt);
     try{
+        const connection = await pool.getConnection();
         const insertuser = 'INSERT INTO `Users` (`username`, `password_hash`) VALUES (?,?)';
         const [rows] = await connection.query(insertuser, [username, passwordHash]);
         console.log(rows);
     }catch(error){
         console.error(error);
-    }
+    } finally {if (connection) {connection.release()}}
 
 }
 
 const compareuser = async (username, password) => {
-    const grabUser='SELECT * FROM `Users` WHERE `username` = ?';
-    const [rows] = await connection.query(grabUser, username)
-    if (rows.length === 0){
-        return(false)} 
-    else {
-        let authenticationprocess = await bcrypt.compare(password, rows[0].password_hash);
-        userID = rows[0].id;
-        return {authenticationprocess, userID };
+    try{
+        const connection = await pool.getConnection();
+        const grabUser='SELECT * FROM `Users` WHERE `username` = ?';
+        const [rows] = await connection.query(grabUser, username)
+        if (rows.length === 0){
+            return(false)} 
+        else {
+            let authenticationprocess = await bcrypt.compare(password, rows[0].password_hash);
+            userID = rows[0].id;
+            return {authenticationprocess, userID };
+        } 
+    } catch (error) {console.error(error)
+
+    } finally {
+        if(connection) {
+            connection.release();
+        }
     }
     // return(compareHash);
 }
 
 const sendMessage = async (user1, user2, content) => {
     try{
+    const connection =pool.getConnection();
     const messageData = 'INSERT INTO `messages` (`sender_id`, `receiver_id`, `content`) VALUES (?,?,?)';
     const [rows] = await connection.query(messageData, [user1,user2,content]);
-    }catch(error){console.error(error)} 
+    }catch(error){console.error(error)
+        
+    } finally {
+        if(connection) {
+            connection.release();
+        }
+    }
 };
 
 const getMessages = async(user1,user2) => {
     try{
+        const connection =pool.getConnection();
         const messagesData = 'SELECT * FROM `messages` WHERE(`sender_id`  = ? AND `receiver_id` = ?) OR (`sender_id` = ? AND `receiver_id` = ?)';
         const [rows] = await connection.query(messagesData, [user1,user2,user2,user1]);
         return(rows);
-    }catch(error){console.error(error)}
+    }catch(error){console.error(error)
+    } finally {
+        if(connection) {
+            connection.release();
+        }
+    }
 }
 
 module.exports = {ConnectingtoDB, findAll,compareuser ,RegisterNewUser,userArr,userID,sendMessage, getMessages};
